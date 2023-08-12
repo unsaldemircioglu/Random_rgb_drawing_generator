ae57135e6ed19bf2db029bcdde893f0bfc349bc8
Wscosmic <142059934+Wscosmic@users.noreply.github.com>
2023-08-13 00:36:28 +0300

    Update Generate_Random_RGB_Colours.py

--- Generate_Random_RGB_Colours.py
+++ Generate_Random_RGB_Colours.py
@@ -1,22 +1,23 @@
-import turtle as t
-import random
+import turtle as t # Turtle kütüphanesi t olarak adlandırıyoruz.
+import random 
 
-tim = t.Turtle()
-t.colormode(255)
-t.speed("fastest")
+tim = t.Turtle() 
+t.colormode(255) # Rgb olmasını isteidğimiz için
+t.speed("fastest") # Hızlandırmak için
 
 def random_color():
-    r = random.randint(0, 255)
+    r = random.randint(0, 255) # 0,255 aralığı rgb renk aralığıdır
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     random_color = (r , g , b)
-    return  random_color
+    return  random_color 
 
 #colours = ["CornflowerBlue","DarkOrchid","IndianRed","DeepSkyBlue","LightSeaGreen","wheat","SlateGray","SeaGreen"]
-directions = [0, 90, 180, 270] # her bir rakam pusuladaki değerlerle eşşittir.
-tim.pensize(15)
 
-for _ in range(1200):
-    tim.color(random_color())
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
+directions = [0, 90, 180, 270] # her bir rakam pusuladaki değerlerle eşitir ve tim objemizin yani turtleımız'ın konumunu belirler sağa dön yukarı dön gibi.
+tim.pensize(15) # tim objemizin yani Turtlımızın çizim yaparkenki çizgi büyüklüğü.
+
+for _ in range(1200): # 1200 kez dönmesini istediğim için isteğe bağlı daha az veya fazla dönmesini sağlayabiliriz.
+    tim.color(random_color()) # tim yani turtle'ın rengini belirliyoruz ama rastgele olmmasını istediğimiz için yukarıdaki random_color() olarak ayarlıyoruz.
+    tim.forward(30) # Her bir renk'te 30 adım git gibi
+    tim.setheading(random.choice(directions)) # pozisyonu'nu ayarlıyoruz rastgele olmasını istediğimiz için random.choice() kodu ile listeden rastgele konum/sayı çekiyoruz bu özelliğide random kütüphanesi sayesinde yapabiliyoruz.
